428bee35d4d6853abdc444157257f886f10d0db3
Roko Musulin <thecrtyy@gmail.com>
2017-03-22 19:50:48 +0100

    Dodan korisnik controller + radi :)

--- MenzaPP/Controllers/KorisnikController.cs
+++ MenzaPP/Controllers/KorisnikController.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 
 namespace MenzaPP.Controllers
 {
     public class KorisnikController : Controller
     {
+        private menzappEntities db = new menzappEntities();
+        
         // GET: Korisnik
         public ActionResult Index()
         {
-            return View();
+            if(User.Identity.IsAuthenticated)
+            {
+                var userEmail = User.Identity.GetUserName();
+                Korisnik korisnik = db.Korisniks.Where(x => x.email == userEmail).First();
+                return View(korisnik);
+            }
+            return View("Error");            
         }
     }
 }
